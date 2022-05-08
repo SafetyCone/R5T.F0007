afe9f3e216861d9ce2ad594d96f1395723800bec
davidcoats <david@getsafetycone.com>
2022-05-08 16:04:35 -0700

    Initial commit of source-stub.

---
+++ source/R5T.F0007.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.F0007.Construction
+{
+    /// <summary>
+    /// Construction project for Generation, composite, functionality for Roslyn syntax elements..
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
---
+++ source/R5T.F0007/Code/Classes/Class1.cs
@@ -0,0 +1,9 @@
+using System;
+
+
+namespace R5T.F0007
+{
+    public class Class1
+    {
+    }
+}
